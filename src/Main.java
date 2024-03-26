1befb09fa9f46870ef9a5dc4cd4d1a4dbdb69d4d
host <vrachea676@gmail.com>
2024-03-26 19:37:36 +0300

    pull_req

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
+import Logger.Logger;
+import Operations.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        ComplexNumber num1 = new ComplexNumber(2, 3);
+        ComplexNumber num2 = new ComplexNumber(4, 5);
+
+        Calculator calculator = new Calculator();
+
+        ComplexNumber sum = calculator.add(num1, num2);
+        Logger.log("Сумма: " + sum.getReal() + " + " + sum.getImaginary() + "i");
+
+        ComplexNumber product = calculator.multiply(num1, num2);
+        Logger.log("Произведение: " + product.getReal() + " + " + product.getImaginary() + "i");
+
+        try {
+            ComplexNumber quotient = calculator.divide(num1, num2);
+            Logger.log("Частное: " + quotient.getReal() + " + " + quotient.getImaginary() + "i");
+        } catch (ArithmeticException e) {
+            Logger.log("Ошибка: " + e.getMessage());
+        }
     }
 }
